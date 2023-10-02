2b306ce85717d0810d23049e39453438cda61341
Sandeep SKHR <sandeephrkumar@gmail.com>
2023-10-02 18:47:10 +0530

    updated_startfile

--- start.py
+++ start.py
@@ -1,1 +1,11 @@
+from datetime import datetime
+c = datetime.now()
 print(" i am from start")
+current_time = c.strftime('%Y%m%d%H%M%S')
+print(c)
+print(current_time)
+filename = "file_" + current_time +".txt"
+f = open(filename, "a")
+f.write("Now the file has more content!")
+f.close()
+
